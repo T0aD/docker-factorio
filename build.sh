5b758d33b13f858522275a5ef94c6df1ec4e18c9
toad <toad@lescigales.org>
2016-11-19 16:45:38 +0100

    changed build + upgraded to 0.14.20

--- build.sh
+++ build.sh
@@ -1,4 +1,14 @@
 #! /bin/bash
 
+VERSION=0.14.20
+
+wget https://www.factorio.com/get-download/$VERSION/headless/linux64 -O factorio_headless_x64_$VERSION.tar.gz
+tar xzvf factorio_headless_x64_$VERSION.tar.gz
+rm -v factorio_headless_x64_$VERSION.tar.gz
+du -sh ./factorio
+strip ./factorio/bin/x64/factorio
+du -sh ./factorio
+
 docker build -t factorio .
+docker build -t factorio:$VERSION .
 docker images factorio
--- usr/sbin/entry.sh
+++ usr/sbin/entry.sh
@@ -1,7 +1,5 @@
 #! /bin/bash
 
-#set -x
-
 NAME=$1
 
 if [ -z $NAME ]; then
@@ -29,31 +27,6 @@ ls -lh $savename
 echo "Rest of the saves:"
 ls -lt /usr/local/factorio/saves
 
-$bin --start-server $NAME --no-auto-pause
-
-exit
-
-cat <<EOF 
-General options:
-
-  -h [ --help ]               display help
-  --version                   show version information
-  -c [ --config ] PATH        config file to use
-
-Running options:
-  -s [ --map2scenario ] arg   map to scenario conversion
-  --apply-update arg          immediately apply update package
-  --create SAVE               create a new map
-  --start-server SAVE         start a multiplayer server
-
-Server options:
-  --latency N                 multiplayer server latency, in ticks
-  --latency-ms N              multiplayer server latency, in milliseconds
-  --autosave-interval N (=2)  server autosave interval, in minutes
-  --autosave-slots N (=3)     server autosave slots
-  --disallow-commands         disallow use of the command console
-  --peer-to-peer              use peer-to-peer communication
-  --no-auto-pause             don't automatically pause when no players are 
-                              connected
-EOF
-
+echo "Starting..."
+set -x
+$bin --start-server $NAME
